02ffe0c747171374b9ea83f98430732a9177f8ec
Subramaniyam <subramaniyam73@gmail.com>
2021-05-13 18:46:23 +0530

    Initialize server and add models

---
+++ env.example.js
@@ -0,0 +1,8 @@
+const MONGO_URI = ''; //REPLACE WITH YOUR MONGODB CONNECTION STRING
+const port = 3000; //REPLACE WITH PORT NUMBER FOR SERVER
+const ip = ''; // REPLACE WITH YOUR IP ADDRESS
+
+module.exports = {
+    MONGO_URI,
+    port,
+};
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const config = require('./env');
+
+const port = config.port || 3000;
+
+mongoose.connect( config.MONGO_URI , {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('MongoDB Connection Successful !');
+})
+.catch((err) => {
+    console.log('Database Connection Error' + err);
+});
+
+const routes = require('./routes/index.router');
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}/`);
+});
